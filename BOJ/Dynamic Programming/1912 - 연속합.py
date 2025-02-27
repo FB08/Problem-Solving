18e6a73a82f7aa7ed65bbac3802ce4bb7b0d361a
FB08 <yhk9307@naver.com>
2025-02-27 14:41:04 +0900

    Add 1912 - 연속합.py

--- BOJ/Dynamic Programming/1912 - 연속합.py
+++ BOJ/Dynamic Programming/1912 - 연속합.py
@@ -1,2 +1,8 @@
 import sys
-input = map(sys.stdin.read().split())
+input = sys.stdin.readline
+n = int(input())
+dp = [0] * n
+numbers = list(map(int, input().split()))
+for i in range(n):
+    dp[i] = max(dp[i-1]+numbers[i], numbers[i])
+print(max(dp))
