85e4ad380ce0de36ed1ef362d6acef4b4df1ed41
FB08 <yhk9307@naver.com>
2025-03-14 13:59:41 +0900

    Add 14940 - 쉬운 최단거리.py(complete)

--- BOJ/Graph/14940 - 쉬운 최단거리.py
+++ BOJ/Graph/14940 - 쉬운 최단거리.py
@@ -3,24 +3,27 @@ def solution():
     input = sys.stdin.readline
     n, m = map(int, input().split())
     arr = []
-    for _ in range(n):
-        arr.append(list(map(lambda x: [0, -1, 0][int(x)], input().split())))
+    for i in range(n):
+        arr.append([[0, -1, 2][int(x)] for x in input().split()])
         if 2 in arr[-1]:
-            start = (n, arr[-1].index(2))
+            start = (i, arr[-1].index(2))
     # bfs
-    visit = [[0]*m for _ in range(m)]
+    visit = [[0]*m for _ in range(n)]
+    arr[start[0]][start[1]] = 0
     q = [start]
     while q:
         x = q.pop(0)
         r = arr[x[0]][x[1]]+1
         for i,j in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             a,b = x[0]+i, x[1]+j
+            if a<0 or b<0 or a==m or b==n: continue
             if arr[a][b] and not visit[a][b]:
                 arr[a][b] = r
+                visit[a][b] = 1
                 q.append((a, b))
 
     for i in range(n):
-        sys.stdout.write(' '.join(map(str, arr[n]))+'\n')
+        sys.stdout.write(' '.join(map(str, arr[i]))+'\n')
 
 solution()
     
