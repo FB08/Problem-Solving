ab127cbaa27937d23f786598512329d531a144a9
FB08 <109363106+FB08@users.noreply.github.com>
2025-03-25 10:47:52 +0900

    Update 7576 - 토마토.py

--- BOJ/Graph/7576 - 토마토.py
+++ BOJ/Graph/7576 - 토마토.py
@@ -3,24 +3,24 @@ def solution():
     input = map(int, os.read(0, os.fstat(0).st_size).split())
     a, b = next(input), next(input)
     G = [set(), set()] # [안자람, 자람]
-    for x in range(b):
-        for y in range(a):
-            k = next(input)
-            if k != -1:
-                G[k].add((x, y))
+    for h in range(next(input)):
+      for x in range(b):
+          for y in range(a):
+              k = next(input)
+              if k != -1:
+                  G[k].add((x, y, h))
     day = 0
     while G[1] and G[0]:
         day += 1
         temp = []
         while G[1]:
             x = G[1].pop()
-            for a,b in [(0,1),(0,-1),(1,0),(-1,0)]:
-                y = (x[0]+a, x[1]+b)
+            for a,b,c in [(0,1,0),(0,-1,0),(1,0,0),(-1,0,0),(0,0,-1),(0,0,1)]:
+                y = (x[0]+a, x[1]+b, x[2]+c)
                 if y in G[0]:
                     G[0].discard(y)
                     temp.append(y)
-        G[1] = temp
+        G[1] = set(temp)
     os.write(1, str(-1 if G[0] else day).encode())
     os._exit(0)
 solution()
-        
